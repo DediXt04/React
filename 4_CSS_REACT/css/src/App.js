3d891d4cd20f25b865176d22531c989d7788b1c4
DediXt04 <dedi.aqav@gmail.com>
2025-05-08 14:18:13 -0300

    aaaaa

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MyComponent from './components/MyComponent';
+import { useState } from 'react';
+import Title from './components/Title';
 
 function App() {
+  const n = 15;
+  const [name] = useState("AAA");
+  const redTitle = false;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1>React com css</h1>
+      <MyComponent />
+      <p>paragrafo main</p>
+      <p style={{color: "magenta", padding: "25px", borderTop: "2px solid red"}}>
+        este elemento foi estilizado de forma inline
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h2 style={n<10 ? ({color: "purple"}) : ({color:"pink"})}>CSS dinamico</h2>
+      <h2 style={n>10 ? ({color: "purple"}) : ({color:"pink"})}>CSS dinamico</h2>
+      <h2 style={name === "AAA" ? ({color: "green", backgroundColor:"#000"}) : ({color:"pink"})}>Teste nome</h2>
+
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Este titulo vai ter classe dinamica</h2>
+
+      <h2>
+        <Title />
+      </h2>
     </div>
   );
 }
---
+++ 4_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import "./MyComponent.css";
+import React from 'react';
+
+const MyComponent = () => {
+    return (
+        <div>
+            <h1>aaaaaaa</h1>
+            <p>paragrafo para componente</p>
+            <p className="my-comp-p"> Este tbm é do componente</p>
+        </div>
+    );
+};
+
+export default MyComponent;
