baf27037f755c95e5d8552c5bb38ddf80fdafc17
DediXt04 <dedi.aqav@gmail.com>
2025-05-12 15:12:17 -0300

    hahaha

---
+++ 6_SECRET_WORD/secret_word/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordslist = {
+    carro: ["Motor", "Porta", "Roda", "Volante"],
+    casa: ["Janela", "Porta", "Telhado", "Parede"],
+    cachorro: ["Rabo", "Pata", "Orelha", "Nariz"],
+    fruta: ["Banana", "Melancia", "Laranja", "Uva"],
+    computador: ["Teclado", "Mouse", "Monitor", "Processador"],
+    escritorio: ["Mesa", "Cadeira", "Papel", "Caneta"],
+}
