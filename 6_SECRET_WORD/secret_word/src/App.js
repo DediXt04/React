5f7dca838829c457c53dadf2ac9737d506efaa00
DediXt04 <dedi.aqav@gmail.com>
2025-05-13 11:50:55 -0300

    aasasa

--- 6_SECRET_WORD/secret_word/src/App.js
+++ 6_SECRET_WORD/secret_word/src/App.js
@@ -1,9 +1,30 @@
+//CSS
 import './App.css';
 
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// Data
+import { wordsList } from './data/words';
+
+// Components
+import { StartScreen } from './components/StartScreen';
+
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' },
+];
+
 function App() {
+
+  const [gameStage, setGameStage] = useState(stages[0].name);
+
   return (
     <div className="App">
-      <h2>Secret Word</h2>
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <h1>Game</h1>}
+      {gameStage === 'end' && <h1>End</h1>}
     </div>
   );
 }
---
+++ 6_SECRET_WORD/secret_word/src/components/StartScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./StartScreen.css"
+
+export const StartScreen = () => {
+  return (
+    <div className="start">
+        <h1>Secret Word</h1>
+        <p>Clique no botão abaixo para comecar a jogar</p>
+        <button>Começar o jogo</button>
+    </div>
+  )
+}
