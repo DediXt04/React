5a940e1c5c07a17e3fceaba824f9f203f174c831
DediXt04 <dedi.aqav@gmail.com>
2025-05-23 17:39:55 -0300

    adsda

--- 10_MiniBlog/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_MiniBlog/miniblog/src/pages/Dashboard/Dashboard.js
@@ -10,6 +10,7 @@ import {useFetchDocument} from '../../hooks/useFetchDocuments'
 const Dashboard = () => {
   const {user} =useAuthValue()
   const uid = user.uid
+  const posts = [];
 
 
 
@@ -17,6 +18,16 @@ const Dashboard = () => {
     <div>
         <h2>Dashboard</h2>
         <p>Gerencie os seus posts</p>
+        {posts && posts.length === 0 ? (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">Criar primeiro post</Link>
+          </div>
+        ) : (
+          <div>
+            <p> Tem posts! </p>
+          </div>
+        ) }
     </div>
   )
 }
