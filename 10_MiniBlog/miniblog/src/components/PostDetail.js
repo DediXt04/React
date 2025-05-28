4fff69d5e22a754285bea498e3e697c2a3cf4106
DediXt04 <dedi.aqav@gmail.com>
2025-05-28 15:08:27 -0300

    171

--- 10_MiniBlog/miniblog/src/components/PostDetail.js
+++ 10_MiniBlog/miniblog/src/components/PostDetail.js
@@ -1,23 +1,34 @@
-import styles from './PostDetail.module.css'
+import styles from './PostDetail.module.css';
+import { Link, useNavigate } from 'react-router-dom';
+import React from 'react';
 
-import { Link } from 'react-router-dom'
+const PostDetail = ({ post }) => {
+  const navigate = useNavigate();
 
-import React from 'react'
+  const handleDoubleClick = () => {
+    navigate(`/posts/${post.id}`);
+  };
 
-const PostDetail = ({post}) => {
   return (
     <div className={styles.post_detail}>
-        <img src={post.image} alt={post.title} className={styles.img} />
-        <h2>{post.title}</h2>
-        <p className={styles.createdby}>{post.createdBy}</p>
-        <div className={styles.tags}>
-            {post.tags.map((tag)=>(
-                <p key={tag}><span>#</span>{tag}</p>
-            ))}
-        </div>
-        <Link to={`/posts/${post.id}`} className='btn btn-outline'>Ler</Link>
+      <img
+        src={post.image}
+        alt={post.title}
+        className={styles.img}
+        onDoubleClick={handleDoubleClick}
+      />
+      <h2>{post.title}</h2>
+      <p className={styles.createdby}>{post.createdBy}</p>
+      <div className={styles.tags}>
+        {post.tags.map((tag) => (
+          <p key={tag}>
+            <span>#</span>{tag}
+          </p>
+        ))}
+      </div>
+      <Link to={`/posts/${post.id}`} className='btn btn-outline'>Ler</Link>
     </div>
-  )
-}
+  );
+};
 
-export default PostDetail
+export default PostDetail;
--- 11_HOOKS/reacthooks/src/components/HookUseReducer.js
+++ 11_HOOKS/reacthooks/src/components/HookUseReducer.js
@@ -1,11 +1,47 @@
-import { useReducer } from "react"
+import { useReducer, useState } from "react"
 
 const HookUseReducer = () => {
 
-    const [number, dispatch] = useReducer((state, action) =>{
+    const [number, dispatch] = useReducer((state, action) => {
         return Math.random(state)
     })
-    
+
+    const initialTasks = [
+        { id: 1, text: "Dormir" },
+        { id: 2, text: "Comer" }
+    ]
+
+    const taskReducer = (state, action) => {
+        switch (action.type) {
+            case "ADD":
+                const newTask = {
+                    id: Math.floor(Math.random() * 1000),
+                    text: taskText
+                }
+                setTastText('')
+
+                return [...state, newTask]
+
+            case "DELETE":
+                return state.filter((task) => task.id !== action.id)
+            default:
+                return state;
+        }
+
+    }
+    const [taskText, setTastText] = useState('')
+    const [tasks, dispatchTasks] = useReducer(taskReducer, initialTasks)
+
+
+    const handleSumbit = (e) => {
+        e.preventDefault()
+
+        dispatchTasks({ type: "ADD" })
+    }
+
+    const removeTask = (id) => {
+        dispatchTasks({type: "DELETE", id})
+    }
 
 
     return (
@@ -13,6 +49,15 @@ const HookUseReducer = () => {
             <h2>useReducer</h2>
             <p>Número: {number}</p>
             <button onClick={dispatch}>Alterar número!</button>
+            <h3>Tarefas: </h3>
+            <form onSubmit={handleSumbit}>
+                <input type="text" onChange={(e) => setTastText(e.target.value)} value={taskText}  placeholder="Digite a nova tarefa!"/>
+                <input type="submit"/>
+            </form>
+            {tasks.map((task) => {
+                console.log(task.id, task.text);
+                return <li key={task.id} onDoubleClick={()=> removeTask(task.id)}>{task.text}</li>;
+            })}
             <hr />
         </div>
     )
