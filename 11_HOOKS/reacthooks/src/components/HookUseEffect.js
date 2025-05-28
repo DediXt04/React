b7632c1c596021e8abd5562db102857714ee19e7
DediXt04 <dedi.aqav@gmail.com>
2025-05-28 16:35:00 -0300

    174

--- 11_HOOKS/reacthooks/src/components/HookUseEffect.js
+++ 11_HOOKS/reacthooks/src/components/HookUseEffect.js
@@ -4,6 +4,7 @@ const HookUseEffect = () => {
 
   useEffect(()=>{
     console.log("Estou sendo executado!")
+    
   })
 
   const [number, setNumber] = useState(1)
@@ -16,14 +17,23 @@ const HookUseEffect = () => {
     console.log("Serei executado apenas uma vez!")
   },[])
 
+  const [anotherNumber, setAnotherNumber] = useState(0)
+
+  useEffect(()=>{
+    if(anotherNumber > 0){
+    console.log("Sou executado apenas quando o aotherNumber muda!")
+    }
+  },[anotherNumber])
 
   return (
     <div>
       <h1>useEffect</h1>
       <p>Number: {number}</p>
       <button onClick={changeSomething}>Executar!</button>
+      <p>Another Number: {anotherNumber}</p>
+      <button onClick={()=> setAnotherNumber(anotherNumber +1)}>Mudar anotherNumber</button>
 
-
+      <hr />
     </div>
 
   )
