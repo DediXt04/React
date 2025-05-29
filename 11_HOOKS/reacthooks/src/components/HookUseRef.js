fdab467af1c46148c27aed893b281e4ff13dfb9a
DediXt04 <dedi.aqav@gmail.com>
2025-05-29 15:56:00 -0300

    178

--- 11_HOOKS/reacthooks/src/components/HookUseRef.js
+++ 11_HOOKS/reacthooks/src/components/HookUseRef.js
@@ -11,14 +11,31 @@ const HookUseRef = () => {
         numberRef.current = numberRef.current + 1;
     })
 
+    const inputRef = useRef()
+    const [text,setText] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        setText("")
+
+        inputRef.current.focus()
+    }
+
     return (
         <div>
             <h2>UseRef</h2>
+            {"1- useRef"}
             <p>O componente renderizou: {numberRef.current} vezes</p>
             <p>Counter 1: {counter}</p>
-            <button onClick={()=> setCounter(counter +1)}>Contador A</button>
+            <button onClick={() => setCounter(counter + 1)}>Contador A</button>
             <p>Counter 2: {counterB}</p>
-            <button onClick={()=> setCounterB(counterB +1)}>Contador B</button>
+            <button onClick={() => setCounterB(counterB + 1)}>Contador B</button>
+            {"2- useRef Dom"}
+            <form onSubmit={handleSubmit}>
+                <input type="text"  ref={inputRef} value={text} onChange={(e) => setText(e.target.value)}/>
+                <input type="submit" value="Enviar" />
+            </form>
             <hr />
         </div>
     )
