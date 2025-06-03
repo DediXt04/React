bacf9f3b17f76ccc95d86932100a3d11a486f956
DediXt04 <dedi.aqav@gmail.com>
2025-06-03 14:25:16 -0300

    204

--- 12_REACTGRAM/backend/middlewares/imageUpload.js
+++ 12_REACTGRAM/backend/middlewares/imageUpload.js
@@ -5,27 +5,27 @@ const imageStorage = multer.diskStorage({
     destination: (req, file, cb) => {
         let folder = ""
 
-        if(req.baseUrl.includes("users")){
+        if (req.baseUrl.includes("users")) {
             folder = "users"
-        }else if (req.baseUrl.includes("photos")){
+        } else if (req.baseUrl.includes("photos")) {
             folder = "photos"
         }
 
         cb(null, `uploads/${folder}/`)
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now()+path.extname(file.originalname))
+        cb(null, Date.now() + path.extname(file.originalname))
     }
 })
 
-const imageUpload = multer ({
+const imageUpload = multer({
     storage: imageStorage,
-    fileFilter(req, file, cb){
-        if(!file.originalname.match(/\.(png|jpg)$/)){
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(png|jpg)$/)) {
             return cb(new errorMonitor("Por favor, envie apenas png ou jpg!"))
         }
         cb(undefined, true)
     }
 })
 
-module.exports = {imageUpload}
+module.exports = { imageUpload }
--- 12_REACTGRAM/backend/routes/UserRoutes.js
+++ 12_REACTGRAM/backend/routes/UserRoutes.js
@@ -2,16 +2,18 @@ const express = require("express")
 const router = express.Router()
 
 //Controller
-const {register, login, getCurrentUser} = require("../controllers/UseController")
+const {register, login, getCurrentUser, update} = require("../controllers/UseController")
 
 //Middlewares
 const validate = require("../middlewares/handleValidations")
-const {userCreateValidation, loginValidation } = require("../middlewares/userValidations")
+const {userCreateValidation, loginValidation, userUpdateValidation } = require("../middlewares/userValidations")
 const authGuard = require("../middlewares/authGuard")
+const { imageUpload } = require("../middlewares/imageUpload")
 
 //Routes
 router.post("/register",userCreateValidation(), validate, register)
 router.post("/login",loginValidation(), validate, login)
 router.get("/profile",authGuard, getCurrentUser)
+router.put("/", authGuard, userUpdateValidation(), validate, imageUpload.single("profileImage"), update)
 
 module.exports = router
