ad5c5689b59e9faf1d6cfb8f6860389b68ad5bf4
DediXt04 <dedi.aqav@gmail.com>
2025-06-03 12:01:48 -0300

    198

--- 12_REACTGRAM/backend/controllers/UseController.js
+++ 12_REACTGRAM/backend/controllers/UseController.js
@@ -6,14 +6,41 @@ const jwt = require("jsonwebtoken")
 const jwtSecret = process.env.JWT_SECRET
 
 const generateToken = (id) => {
-    return jwt.sign({id}, jwtSecret, {
+    return jwt.sign({ id }, jwtSecret, {
         expiresIn: "7d"
     })
-} 
-
-const register = async(req, res) =>{
-    res.send("Registro")
 }
+
+const register = async (req, res) => {
+    const { name, email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (user) {
+        res.status(422).json({ errors: ["Por favor, utilize outro e-mail"] })
+        return
+    }
+
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    if (!newUser) {
+        res.status(422).json({ errors: ["Houve um erro, por favor tente mais tarde."] })
+        return
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    })
+}
+
 
 module.exports = {
     register,
