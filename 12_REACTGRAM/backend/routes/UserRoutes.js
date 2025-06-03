6654d9f0de85c964d6bbe0c8a351f019b505d1c1
DediXt04 <dedi.aqav@gmail.com>
2025-06-03 13:53:23 -0300

    202

--- 12_REACTGRAM/backend/routes/UserRoutes.js
+++ 12_REACTGRAM/backend/routes/UserRoutes.js
@@ -2,14 +2,16 @@ const express = require("express")
 const router = express.Router()
 
 //Controller
-const {register, login} = require("../controllers/UseController")
+const {register, login, getCurrentUser} = require("../controllers/UseController")
 
 //Middlewares
 const validate = require("../middlewares/handleValidations")
 const {userCreateValidation, loginValidation } = require("../middlewares/userValidations")
+const authGuard = require("../middlewares/authGuard")
 
 //Routes
 router.post("/register",userCreateValidation(), validate, register)
 router.post("/login",loginValidation(), validate, login)
+router.get("/profile",authGuard, getCurrentUser)
 
 module.exports = router
