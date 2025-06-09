3a179ff0713e6a8089afffde0ac7cd5412ea3846
DediXt04 <dedi.aqav@gmail.com>
2025-06-09 13:47:45 -0300

    219

--- 12_REACTGRAM/frontend/src/App.js
+++ 12_REACTGRAM/frontend/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
 
 function App() {
   return (
     <div className="App">
-      <h1>ReactGram</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
