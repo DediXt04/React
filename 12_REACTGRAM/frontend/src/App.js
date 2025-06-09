a749c989fc61aba0d5e644ba0a167a6027057355
DediXt04 <dedi.aqav@gmail.com>
2025-06-09 14:18:09 -0300

    220

--- 12_REACTGRAM/frontend/src/App.js
+++ 12_REACTGRAM/frontend/src/App.js
@@ -2,20 +2,27 @@ import './App.css';
 
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
+//Pages
 import Home from './pages/Home/Home';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
+
+//Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/register" element={<Register/>} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
