af8c1a08b31d2c90daabdb0af366513a87628a79
DediXt04 <dedi.aqav@gmail.com>
2025-06-09 14:50:31 -0300

    222

--- 12_REACTGRAM/frontend/src/pages/Auth/Register.js
+++ 12_REACTGRAM/frontend/src/pages/Auth/Register.js
@@ -1,8 +1,32 @@
 import './Auth.css';
 
+import {Link} from 'react-router-dom';
+
+import { useState, useEffect } from 'react';
+
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  }
+
+
   return (
-    <div>Register</div>
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastra-se para ver fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Nome' />
+        <input type="email" placeholder='E-mail' />
+        <input type="password" placeholder='Senha' />
+        <input type="password" placeholder='Confirme a senha' />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem uma conta? <Link to="/login">Clique aqui</Link>
+      </p>
+    </div>
   )
 }
 
