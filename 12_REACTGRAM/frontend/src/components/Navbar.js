9bd0243927e8d6ce260e791bb7839f00dfa82dcc
DediXt04 <dedi.aqav@gmail.com>
2025-06-10 17:26:24 -0300

    237

--- 12_REACTGRAM/frontend/src/components/Navbar.js
+++ 12_REACTGRAM/frontend/src/components/Navbar.js
@@ -8,10 +8,21 @@ import { useAuth } from '../hooks/useAuth';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
+import {logout, reset} from '../slices/authSlices';
+
 const Navbar = () => {
 
     const { auth } = useAuth();
     const { user } = useSelector((state) => state.auth);
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(logout());
+        dispatch(reset());
+        navigate('/login');
+    }
 
 
     return (
@@ -43,7 +54,7 @@ const Navbar = () => {
                             </NavLink>
                         </li>
                         <li>
-                            <span>Sair</span>
+                            <span onClick={handleLogout}>Sair</span>
                         </li>
 
                     </>
