63e4cddf57844fe8f0ee38752558aaa785f180d9
DediXt04 <dedi.aqav@gmail.com>
2025-06-11 13:07:34 -0300

    239

--- 12_REACTGRAM/frontend/src/services/authService.js
+++ 12_REACTGRAM/frontend/src/services/authService.js
@@ -25,8 +25,29 @@ const logout = () => {
     localStorage.removeItem("user");
 }
 
+// Login an user
+const login = async (data) => {
+    const config = requestConfig("POST", data)
+    try {
+        const res = await fetch(api + "/users/login", config)
+            .then((res) => res.json())
+            .catch((err) => err)
+
+        if (res) {
+            localStorage.setItem("user", JSON.stringify(res))
+        }
+
+        return res;
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 const authService = {
     register,
+    logout,
+    login
 }
 
 export default authService;
