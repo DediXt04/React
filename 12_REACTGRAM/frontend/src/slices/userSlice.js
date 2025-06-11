ee849c83a6dc8bfe836d2df65ec6d64357c44f38
DediXt04 <dedi.aqav@gmail.com>
2025-06-11 17:21:18 -0300

    244

---
+++ 12_REACTGRAM/frontend/src/slices/userSlice.js
@@ -0,0 +1,23 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: {},
+    erro: null,
+    loading: false,
+    message: null
+}
+
+//funcoes
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        resetMessage: (state) => {
+            state.message = null;
+        }
+    },
+})
+
+export const { resetMessage } = userSlice.actions;
+export default userSlice.reducer;
