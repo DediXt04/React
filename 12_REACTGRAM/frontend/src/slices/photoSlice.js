93dd6a9a9c3b8f9a770239192a05e296d26a8963
DediXt04 <dedi.aqav@gmail.com>
2025-06-13 16:50:42 -0300

    260

--- 12_REACTGRAM/frontend/src/slices/photoSlice.js
+++ 12_REACTGRAM/frontend/src/slices/photoSlice.js
@@ -1,4 +1,4 @@
-import {createSlice, createAsyncThunk} from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import photoService from "../services/photoService"
 
 
@@ -11,6 +11,22 @@ const initialState = {
     message: null
 }
 
+export const publishPhoto = createAsyncThunk(
+    "photo/publish",
+    async (photo, thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token
+
+        const data = await photoService.publishPhoto(photo, token)
+
+        if (data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data
+    }
+)
+
 export const photoSlice = createSlice({
     name: "photo",
     initialState,
@@ -18,8 +34,28 @@ export const photoSlice = createSlice({
         resetMessage: (state) => {
             state.message = null
         }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(publishPhoto.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(publishPhoto.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.photo = action.payload;
+                state.photos.unshift(state.photo)
+                state.message = "Foto publicada com sucesso!"
+            })
+            .addCase(publishPhoto.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+                state.photo = {};
+            })
     }
 })
 
-export const {resetMessage} = photoSlice.actions
+export const { resetMessage } = photoSlice.actions
 export default photoSlice.reducer
