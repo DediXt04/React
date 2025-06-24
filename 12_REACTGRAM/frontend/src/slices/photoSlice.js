e878cf3babd8472aa5b79364d0026352ec3c681a
DediXt04 <dedi.aqav@gmail.com>
2025-06-24 15:49:09 -0300

    274

--- 12_REACTGRAM/frontend/src/slices/photoSlice.js
+++ 12_REACTGRAM/frontend/src/slices/photoSlice.js
@@ -68,6 +68,14 @@ export const updatePhoto = createAsyncThunk(
     }
 )
 
+export const getPhoto = createAsyncThunk("photo/getphoto",
+    async(id) => {
+        const data = await photoService.getPhoto(id)
+
+        return data
+    }
+)
+
 export const photoSlice = createSlice({
     name: "photo",
     initialState,
@@ -150,6 +158,16 @@ export const photoSlice = createSlice({
                 state.loading = false;
                 state.error = action.payload;
                 state.photo = {};
+            })
+            .addCase(getPhoto.pending, (state) => {
+                state.loading = true;
+                state.error = false;
+            })
+            .addCase(getPhoto.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.photos = action.payload; // action.payload deve ser um array aqui
             })
     }
 })
