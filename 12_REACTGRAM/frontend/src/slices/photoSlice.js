c247a71a5c009481142f57636a15c3725ca7b1f8
DediXt04 <dedi.aqav@gmail.com>
2025-06-24 13:28:23 -0300

    267? (se pa ta errado)

--- 12_REACTGRAM/frontend/src/slices/photoSlice.js
+++ 12_REACTGRAM/frontend/src/slices/photoSlice.js
@@ -38,6 +38,20 @@ export const getUserPhotos = createAsyncThunk(
     }
 )
 
+export const deletePhoto = createAsyncThunk(
+    "photo/delete",
+    async(id, thunkAPI) => {
+        const token = thunkAPI.getState().auth.user.token;
+        try {
+            const data = await photoService.deletePhoto(id, token);
+            return data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+
+    }
+)
+
 export const photoSlice = createSlice({
     name: "photo",
     initialState,
@@ -79,7 +93,26 @@ export const photoSlice = createSlice({
                 state.loading = false;
                 state.error = action.payload; // O erro retornado pelo rejectWithValue
                 state.photos = []; // Garante que photos seja um array vazio em caso de erro
-            });
+            })
+            .addCase(deletePhoto.pending, (state) => {
+                state.loading = true;
+                state.error = false;
+            })
+            .addCase(deletePhoto.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.photos = state.photos.filter((photo) => {
+                    return photo._id !== action.payload.id
+                })
+
+                state.message = action.payload.message
+            })
+            .addCase(deletePhoto.rejected, (state, action)=> {
+                state.loading = false;
+                state.error = action.payload;
+                state.photo = {};
+            })
     }
 })
 
