7a45d5127266bfe60be1d21ed053545ca7ab34fe
DediXt04 <dedi.aqav@gmail.com>
2025-06-25 13:49:36 -0300

    277

--- 12_REACTGRAM/frontend/src/services/photoService.js
+++ 12_REACTGRAM/frontend/src/services/photoService.js
@@ -93,6 +93,19 @@ const getPhoto = async (id, token) => {
       throw error; // importante lançar o erro para o createAsyncThunk saber
     }
   }
+
+  const like = async(id, token) => {
+
+    const config = requestConfig("PUT", null, token)
+
+    try {
+        const res = await fetch(api + "/photos/like/" + id, config).then((res) => res.json()).catch(err => err)
+
+        return res
+    } catch (error) {
+        console.log(error)
+    }
+  }
   
 
 const photoService = {
@@ -101,6 +114,7 @@ const photoService = {
     deletePhoto,
     updatePhoto,
     getPhoto,
+    like,
 }
 
 export default photoService;
