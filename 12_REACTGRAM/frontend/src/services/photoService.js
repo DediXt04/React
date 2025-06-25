72dacffe7b6b2e6c033cc38fb7b61041e9307988
DediXt04 <dedi.aqav@gmail.com>
2025-06-25 15:13:26 -0300

    281

--- 12_REACTGRAM/frontend/src/services/photoService.js
+++ 12_REACTGRAM/frontend/src/services/photoService.js
@@ -106,6 +106,23 @@ const getPhoto = async (id, token) => {
         console.log(error)
     }
   }
+
+  //add comment
+  const comment = async(data,id,token) => {
+    
+    const config = requestConfig("PUT", data, token)
+
+    try {
+        
+        const res = await fetch(api + "/photos/comment/" + id, config).then((res) => res.json()).catch(err => err)
+
+        return res
+
+    } catch (error) {
+        console.log(error)
+    }
+
+  }
   
 
 const photoService = {
